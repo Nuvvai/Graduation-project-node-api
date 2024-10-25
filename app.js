60a2b08deca6090087c97343d285e0d91a44368f
Mennatallah9 <mennaa619@gmail.com>
2024-10-25 19:20:57 +0300

    remove useNewUrlParser and useUnifiedTopology because they have no effect

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/api', pipelineRoutes);
 const CONNECTION_URL = 'mongodb://localhost:27017/';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL)
     .then(() => console.log("Connected to MongoDB successfully!"))
     .then(() => app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)))
     .catch((error) => console.error(error.message));
