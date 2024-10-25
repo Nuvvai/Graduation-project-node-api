474e1326b1f2cc0899014e332ec251d5edd009fa
Mennatallah9 <mennaa619@gmail.com>
2024-10-25 19:19:37 +0300

    comment to implement later

--- controller/pipelineController.js
+++ controller/pipelineController.js
@@ -1,8 +1,8 @@
 const Pipeline = require('../models/Pipeline');
 const Project = require('../models/Project');
-const jenkins = require('jenkins')({
+// const jenkins = require('jenkins')({
     
-})
+// })
 
 const pipelineController = {
 //TODO: implement createPipeline, triggerBuild, getBuildStatus, getBuildLogs, updatePipeline, stopBuild, deletePipeline
--- routes/pipelineRoutes.js
+++ routes/pipelineRoutes.js
@@ -3,8 +3,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/pipeline', createPipeline);
-router.delete('/pipeline/:id', deletePipeline)
+// router.post('/pipeline', createPipeline);
+// router.delete('/pipeline/:id', deletePipeline)
 //TODO: implement the remaining routes
 
 module.exports = router;
