0d874d4bf32f281a883d5bf6b8f0a43963011e44
Mennatallah9 <mennaa619@gmail.com>
2024-10-25 18:59:14 +0300

    add pipeline controller and routes

---
+++ routes/pipelineRoutes.js
@@ -0,0 +1,10 @@
+const pipelineController = require('../controller/pipelineController');
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/pipeline', createPipeline);
+router.delete('/pipeline/:id', deletePipeline)
+//TODO: implement the remaining routes
+
+module.exports = router;
