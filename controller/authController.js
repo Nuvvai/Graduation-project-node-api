7b178689a766f110379a3f9c5eb2c2e2f6f48f39
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2024-11-02 17:22:15 +0200

    authController changes

--- controller/authController.js
+++ controller/authController.js
@@ -9,12 +9,9 @@ const register_controller = async (req, res) => {
     try {
         const existingUser = await User.findOne({ email: email });
         if (existingUser) return res.status(409).json({ message: 'User already exists' });
-        if (!name || !email || !password) return res.status(406).json({ message: "Not accepted, missing parameter" });
-        else if (email.length < 6 || email.indexOf('@') === -1) return res.status(406).json({ message: 'Invalid email format' }); //not tested yet ...!!
-        else if (password.length < 6) return res.status(406).json({ message: 'Password must be at least 6 characters long' });  //not tested yet ...!!
-        else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}/.test(password)) return res.status(406).json({ message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character' });  //not tested yet ...!!
 
         if (!name || !email || !password) return res.status(406).json({ message: "Not accepted, missing parameter" });
+        else if (name.indexOf('@') !== -1) return res.status(406).json({ message: 'Invalid name can not include \"@\"' }); //not tested yet ...!!
         else if (email.length < 6 || email.indexOf('@') === -1) return res.status(406).json({ message: 'Invalid email format' }); //not tested yet ...!!
         else if (password.length < 6) return res.status(406).json({ message: 'Password must be at least 6 characters long' });  //not tested yet ...!!
         else if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}/.test(password)) return res.status(406).json({ message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character' });  //not tested yet ...!!
@@ -23,26 +20,37 @@ const register_controller = async (req, res) => {
         const newUser = new User({ name, email, password: hashedPassword });
         await newUser.save();
 
-        const token = jwt.sign({ email: newUser.email, id: newUser._id }, 'secret', { expiresIn: '1d' });
+        const token = jwt.sign({ name: existingUser.name, email: newUser.email, id: newUser._id }, 'secret', { expiresIn: '1d' });
         res.cookie('jwt-token', token, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 7 * 24 * 60 * 60 * 1000 });
         res.status(201).json({ result: newUser });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Something went wrong' });
+        res.status(500).json({ message: 'Something went wrong', error: error.message });
     }
 }
 
 const login_controller = async (req, res) => {
-    const { email, password } = req.body;
+    const { nameOrEmail, password } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email: email });
+        if (!nameOrEmail || !password) return res.status(406).json({ message: "Not accepted, missing parameter" });
+
+        let existingUser;
+        if (nameOrEmail.indexOf('@') === -1) {
+            const UserName = nameOrEmail;
+            existingUser = await User.findOne({ name: UserName });
+        } else if (nameOrEmail.indexOf('@') !== -1) {
+            const UserEmail = nameOrEmail;
+            existingUser = await User.findOne({ email: UserEmail });
+        }
+
         if (!existingUser) return res.status(404).json({ message: 'User not found' });
 
         const isMatch = await bcrypt.compare(password, existingUser.password);
         if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser.id }, 'secret', { expiresIn: '1d' });
+        const token = jwt.sign({ name: existingUser.name, email: existingUser.email, id: existingUser.id }, 'secret', { expiresIn: '1d' });
+        res.cookie('jwt-token', token, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 7 * 24 * 60 * 60 * 1000 });
         res.status(200).json({ result: existingUser })
 
     } catch (error) {
