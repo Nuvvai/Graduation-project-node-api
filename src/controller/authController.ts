5862c450c9d41e27e3b9b93ad3e00fb749c2de47
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2024-12-18 01:36:51 +0200

    Changes the userController from JS to TS and documents the userController, authController, and errorHandler

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -2,9 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
-import User from '../models/User';
-import { IUser } from './../models/User';
-
+import User, { IUser } from '../models/User';
 interface RegisterRequestBody {
     name: string;
     email: string;
@@ -18,7 +16,12 @@ interface LoginRequestBody {
 
 /**
  * @author Hazem Sabry
- * @des Controller function for user registration.
+ * @description Controller function for user registration.
+ * @param req.name - The name of the register user.
+ * @param req.email - The email of the register user.
+ * @param req.password - The password of the register user.
+ * @returns A promise that resolves when the registration is successful.
+ * @throws { Error } if failed to encrypt the password, or create new user, or the environment variable JWT_Token is undefined, or failed sign a JWT token.
  * @route GET /auth/register
  * @access public 
  */
@@ -26,7 +29,7 @@ export const register_controller = async (req:Request<{}, {}, RegisterRequestBod
     const { name, email, password }:RegisterRequestBody = req.body;
 
     try {
-        const existingUser = await User.findOne<IUser>({ email: email });
+        const existingUser:IUser | null = await User.findOne<IUser>({ email: email });
         if (existingUser) {
             res.status(409).json({ message: 'User already exists' });
             return;
@@ -52,7 +55,7 @@ export const register_controller = async (req:Request<{}, {}, RegisterRequestBod
         }
 
         const hashedPassword:string = await bcrypt.hash(password, 12);
-        const newUser = new User({ name, email, password: hashedPassword });
+        const newUser:IUser = new User({ name, email, password: hashedPassword });
         await newUser.save();
 
         const secretKey: string | undefined = process.env.JWT_Token;
@@ -61,7 +64,7 @@ export const register_controller = async (req:Request<{}, {}, RegisterRequestBod
             throw new Error('Server error, secret key not found, cannot send token');
         }
 
-        const token = jwt.sign({ id: newUser._id, name: newUser.name, email: newUser.email }, secretKey, { expiresIn: '1d' });
+        const token:string = jwt.sign({ id: newUser._id, name: newUser.name, email: newUser.email }, secretKey, { expiresIn: '1d' });
         res.cookie('jwt-token', token, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 7 * 24 * 60 * 60 * 1000 });
         res.status(201).json({ result: newUser });
     } catch (error) {
@@ -71,7 +74,11 @@ export const register_controller = async (req:Request<{}, {}, RegisterRequestBod
 
 /**
  * @author Hazem Sabry
- * @des Controller function for user login.
+ * @description Controller function for user login.
+ * @param req.nameOrEmail The name or email of the user to login with.
+ * @param req.password The password of the user to login with.
+ * @returns A promise that resolves when the user is logged in successfully.
+ * @throws { Error } - If there is an error fetching the user profile, or the password is failed to encrypt, or the environment variable JWT_Token is undefined, or failed sign a JWT token.
  * @route GET /auth/login
  * @access public 
  */
@@ -81,12 +88,12 @@ export const login_controller = async (req: Request<{}, {}, LoginRequestBody>, r
     try {
         if (!nameOrEmail || !password) res.status(406).json({ message: "Not accepted, missing parameter" });
 
-        let existingUser;
-        if (nameOrEmail.indexOf('@') === -1) {
-            const UserName = nameOrEmail;
+        let existingUser:IUser | null = null;
+        if (nameOrEmail.indexOf('@') === -1) {  //user login with username.
+            const UserName:string = nameOrEmail;
             existingUser = await User.findOne({ name: UserName });
-        } else if (nameOrEmail.indexOf('@') !== -1) {
-            const UserEmail = nameOrEmail;
+        } else if (nameOrEmail.indexOf('@') !== -1) {   //user login with email.
+            const UserEmail:string = nameOrEmail;
             existingUser = await User.findOne({ email: UserEmail });
         }
 
@@ -95,7 +102,7 @@ export const login_controller = async (req: Request<{}, {}, LoginRequestBody>, r
             return;
         }
 
-        const isMatch = await bcrypt.compare(password, existingUser.password);
+        const isMatch:boolean = await bcrypt.compare(password, existingUser.password);
         if (!isMatch) {
             res.status(401).json({ message: 'Invalid credentials' });
             return;
@@ -107,7 +114,7 @@ export const login_controller = async (req: Request<{}, {}, LoginRequestBody>, r
             throw new Error('Server error, secret key not found, cannot send token');
         }
 
-        const token = jwt.sign({ id: existingUser._id, name: existingUser.name, email: existingUser.email }, secretKey, { expiresIn: '1d' });
+        const token:string = jwt.sign({ id: existingUser._id, name: existingUser.name, email: existingUser.email }, secretKey, { expiresIn: '1d' });
         res.cookie('jwt-token', token, { httpOnly: true, secure: true, sameSite: 'none', maxAge: 7 * 24 * 60 * 60 * 1000 });
         res.status(200).json({ result: existingUser })
 
---
+++ src/controller/userController.ts
@@ -0,0 +1,149 @@
+import { compare, hash } from 'bcryptjs';
+import User, { IUser } from '../models/User';
+import { Request, Response, NextFunction } from 'express';
+
+interface IUserRequestParams { 
+    name: string ;
+}
+
+interface IUserResponseBody {
+    newName: string | undefined ,
+    newEmail: string | undefined ,
+    oldPassword: string | undefined ,
+    newPassword: string | undefined ,
+    newPasswordAgain: string | undefined 
+}
+
+/**
+ * @author Hazem Sabry
+ * @author Mennatallah
+ * @description Retrieves the profile of a user by their name.
+ * @param req.params.name - The name of the user to retrieve.
+ * @param res - The response object.
+ * @returns Promise, if callback has been omitted
+ * @throws {Error} - If there is an error fetching the user profile.
+ * @route GET /users/:username
+ * @access private for the authenticated user only
+ */
+const getUserProfile = async (req:Request<IUserRequestParams>, res:Response,next:NextFunction):Promise<void> => {
+    try {
+        const { name }:IUserRequestParams = req.params;
+        const userExists:IUser | null = await User.findOne({ name: name });
+        if (!userExists) {
+            res.status(404).json({ message: 'User not found!' });
+            return;
+        }
+        res.status(200).json(userExists)
+    } catch (error) {
+        next(error);
+    }
+}
+
+/**
+ * @author Hazem Sabry
+ * @author Mennatallah
+ * @description Deletes a user by name.
+ * @param req.params.name - The name of the user to delete.
+ * @param res - The response object.
+ * @returns A promise that resolves when the user is deleted.
+ * @throws {Error} - If there is an error fetching the user profile or deleting it.
+ * @route DELETE /users/:username
+ * @access private for the authenticated user only
+ */
+const deleteUser = async (req:Request<IUserRequestParams>, res:Response, next:NextFunction):Promise<void> => {
+    try {
+        const { name }:IUserRequestParams = req.params;
+        const userExists:IUser | null = await User.findOne({ name: name });
+        if (!userExists) {
+            res.status(404).json({ message: 'User not found!' });
+            return;
+        }
+        await User.findOneAndDelete(userExists) // might edit this
+        res.status(200).json({ message: "User deleted successfully!" });
+    } catch (error) {
+        next(error);
+    }
+}
+
+/**
+ * @author Hazem Sabry
+ * @author Mennatallah
+ * @description Updates the user profile with the provided information (selected by name).
+ * @param req.params.name - The name of the user to update.
+ * @param req.body.newName - The new name for the user.
+ * @param req.body.newEmail - The new email for the user.
+ * @param req.body.oldPassword - The old password for verification.
+ * @param req.body.newPassword - The new password for the user.
+ * @param req.body.newPasswordAgain - The new password again for confirmation.
+ * @param res - The response object.
+ * @returns A promise that resolves when the profile is updated.
+ * @throws {Error} - If there is an error fetching the user profile or error saving the update.
+ * @route PUT /users/:username
+ * @access private for the authenticated user only
+ */
+const updateUserProfile = async (req:Request<IUserRequestParams,{},IUserResponseBody>, res:Response, next:NextFunction):Promise<void> => {
+    try {
+        const { name }:IUserRequestParams = req.params;
+        const userExists:IUser | null = await User.findOne({ name: name });
+        if (!userExists) {
+            res.status(404).json({ message: 'User not found!' });
+            return;
+        }
+
+        const user: IUser = userExists;
+        
+        const { newName, newEmail, oldPassword, newPassword, newPasswordAgain }: IUserResponseBody = req.body;
+        if (!newName && !newEmail && !oldPassword && !newPassword && !newPasswordAgain) {
+            res.status(400).json({ message: "No information provided to update." });
+            return;
+        }
+
+        if (newName) {
+            user.name = newName;
+        }
+        if (newEmail) {
+            user.email = newEmail;
+        }
+        if (newPassword && newPasswordAgain && oldPassword && (newPassword === newPasswordAgain)) {
+            const isMatch = await compare(oldPassword, user.password);
+            if (!isMatch) {
+                res.status(400).json({ message: 'Incorrect old password!' });
+                return;
+            }
+            const hashedPassword = await hash(newPassword, 12);
+            user.password = hashedPassword;
+        } else if (newPassword && !oldPassword) {
+            res.status(400).json({ message: "Please enter old password!" });
+            return;
+        } else if (newPassword != newPasswordAgain) {
+            res.status(400).json({ message: "Passwords don't match!" });
+            return;
+        }
+        await user.save();
+        res.status(200).json({ message: "Profile updated successfully!", user })
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+/**
+ * @author Hazem Sabry
+ * @author Mennatallah
+ * @description Retrieves all users from the database, excluding their passwords.
+ * @returns A promise that resolves when all users are retrieved.
+ * @throws {Error} - If there is an error fetching the users.
+ * @route GET /users/:username
+ * @access private admin only
+ */
+const getAllUsers = async (req:Request<{},{},{},{},{}>, res:Response,next:NextFunction):Promise<void> => {
+    try {
+        const users = await User.find().select('-password');//exclude password
+        res.json(users)
+    } catch (error) {
+        next(error);
+    }
+}
+
+
+export default { getUserProfile, deleteUser, updateUserProfile, getAllUsers };
