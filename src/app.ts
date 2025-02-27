47a391bec7064128448d61cbf01a022eb4773117
Mennatallah9 <mennaa619@gmail.com>
2025-02-27 17:30:54 +0200

    move server initialization to a new server.ts file and update package.json scripts

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,6 @@ const PORT:PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
     .then(() => console.log("Connected to MongoDB successfully!"))
-    .then(() => app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)))
     .catch((error: { message: any; }) => console.error(error.message));
 
 export default app;
